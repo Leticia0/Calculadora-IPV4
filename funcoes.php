cddeeec619c787235685c12b27f3b9779e746c81
Leticia dos Santos <leticia_Santos250@hotmail.com>
2019-08-29 15:55:14 -0300

    atualizações

--- funcoes.php
+++ funcoes.php
@@ -1,13 +1,6 @@
 <?php
 
-
-
-
-$mascara = 0;
-/*
-  if ($mascara<24) {
-        echo "Digite uma máscara entre 24 e 32";
-    }else{*/
+	$mascara = 0;
 
 
     function qtd_redes($mascara)
@@ -22,6 +15,7 @@ $mascara = 0;
         return $qtd_redes;
     }
 
+
     function qtd_enderecos($mascara)
     {
         $total = 32;
@@ -32,8 +26,6 @@ $mascara = 0;
     }
 
 
-    
-      
     function enderecos_subrede($mascara) {
         $qtde = qtd_redes($mascara);
         $qtde_enderecos = qtd_enderecos($mascara);
@@ -47,6 +39,7 @@ $mascara = 0;
         }
         return $resp;
     } 
+
 
     function enderecos_broadcast($mascara) {
         $qtde = qtd_redes($mascara);
@@ -72,34 +65,36 @@ $mascara = 0;
         $qtd_hosts= $qtd_hosts-2;
 
         
-
-        if ($qtd_hosts<0) {
+        if ($qtd_hosts<0){
             echo "Não";
         }else{
             return $qtd_hosts;
         }
     }
 
-    function primeiro_host_rede($quarta_trinca, $mascara)
-    {
+    function primeiro_host_rede($quarta_trinca, $mascara){
+       
         $q = qtd_enderecos($mascara);
 
         $posicao_rede = (int) ($quarta_trinca/$q);
 
         $primeiro_endereco = $posicao_rede * $q;
+
         $primeiro_host = $primeiro_endereco + 1;
 
         return $primeiro_host;
     }
 
-    function ultimo_host_rede($quarta_trinca, $mascara)
-    {
+    function ultimo_host_rede($quarta_trinca, $mascara){
+
         $q = qtd_enderecos($mascara);
 
         $posicao_rede = (int) ($quarta_trinca/$q);
 
         $primeiro_endereco = $posicao_rede * $q;
+       
         $calculo = $primeiro_endereco + $q;
+       
         $ultimo_host = $calculo - 1;
 
         return $ultimo_host;
@@ -108,13 +103,10 @@ $mascara = 0;
     function calculo_mascara($mascara_decimal){
 
         $mascara = '255.255.255';
-        $bits_setados = $mascara_decimal - 24;
 
-//        $bits_zerados = 8 - $bits_setados;
+        $bits_setados = 32-$mascara_decimal;
 
-
-        $quarta_trinca = pow(2, $bits_setados);
-        $quarta_trinca -= 1;
+        $quarta_trinca = 256 - pow(2, $bits_setados);
 
         $mascara_final = $mascara.".".$quarta_trinca;
 
@@ -124,24 +116,23 @@ $mascara = 0;
 
     function define_classe($primeira_trinca){
         $classe = "";
-        if ($primeira_trinca > 0 and $primeira_trinca < 127){
+        if ($primeira_trinca > 0 and $primeira_trinca < 128){
             $classe = "A";
-        }elseif ($primeira_trinca > 127 and $primeira_trinca < 192){
+        }elseif ($primeira_trinca > 127 and $primeira_trinca < 190){
             $classe = "B";
         }elseif ($primeira_trinca > 191 and $primeira_trinca < 224){
             $classe = "C";
-        }elseif ($primeira_trinca > 223 and $primeira_trinca < 240){
+        }elseif ($primeira_trinca > 223 and $primeira_trinca < 238){
             $classe = "D";
-        }elseif ($primeira_trinca > 239 and $primeira_trinca < 256){
+        }elseif ($primeira_trinca > 239 and $primeira_trinca < 254){
             $classe = "E";
         }else{
-            $classe = "Tente Novamente. Houve um erro";
+            $classe = "Não foi possível calcular";
         }
 
         return $classe;
     }
-	
-
+    
 
     function privacidade_rede($primeira_trinca, $segunda_trinca){
           $privacidade = "";
@@ -158,43 +149,48 @@ $mascara = 0;
           return $privacidade;
     }
 
-if ($_POST["acao"] == "enviar") {
-    $ip1 = $_POST["ip1"];
-    $ip2 = $_POST["ip2"];
-    $ip3 = $_POST["ip3"];
-    $ip4 = $_POST["ip4"];
-    $mascara = $_POST["mascara"];
-    echo "<br>";
+	if ($_POST["acao"] == "enviar") {
+	    $ip1 = $_POST["ip1"];
+	    $ip2 = $_POST["ip2"];
+	    $ip3 = $_POST["ip3"];
+	    $ip4 = $_POST["ip4"];
+	    $mascara = $_POST["mascara"];
+	    echo "<br>";
 
 
     if ($ip1 != NULL and $ip2 != NULL and $ip3 != NULL and $ip4 != NULL and $mascara != NULL){
        
 
-       	function validacao($primeira_trinca){
-	        $val = "";
-	        if ($primeira_trinca > 0 and $primeira_trinca < 127){
-	            $val = "IP Válido";
-	        }elseif ($primeira_trinca > 127 and $primeira_trinca < 192){
-	            $val = "IP Válido";
-	        }elseif ($primeira_trinca > 191 and $primeira_trinca < 224){
-	            $val = "IP Válido";
-	        }elseif ($primeira_trinca > 223 and $primeira_trinca < 240){
-	            $val = "IP Válido";
-	        }elseif ($primeira_trinca > 239 and $primeira_trinca < 256){
-	            $val = "válido";
-	        }else{
-	            $val = "inválido";
-	        }
+        function validacao($primeira_trinca){
+            $val = "";
+            if ($primeira_trinca > 0 and $primeira_trinca < 127){
+                $val = "válido";
+            }elseif ($primeira_trinca > 127 and $primeira_trinca < 192){
+                $val = "válido";
+            }elseif ($primeira_trinca > 191 and $primeira_trinca < 224){
+                $val = "válido";
+            }elseif ($primeira_trinca > 223 and $primeira_trinca < 240){
+                $val = "válido";
+            }elseif ($primeira_trinca > 239 and $primeira_trinca < 256){
+                $val = "válido";
+            }else{
+                $val = "inválido";
+            }
 
         return $val;
     }
 
     if ($mascara>32 || $mascara<24) {
-    	echo "Insira um CIDR entre 24 e 32";
+
+        echo "Insira um CIDR entre 24 e 32";
+
     }elseif($ip1>255 || $ip1<1 || $ip1<0){
-    	echo "Endereço IP Inválido";
-    	}else{
-  		$val = validacao($ip1);
+
+        echo "Endereço IP inválido";
+
+        }else{
+
+        $val = validacao($ip1);
         echo "Esse IP é " . $val;
         echo "<br>";
         echo "<br>";
@@ -213,26 +209,14 @@ if ($_POST["acao"] == "enviar") {
         echo "<br>";
         echo "<br>";
 
-
-       
         $hosts = qtd_hosts($mascara);
         echo " Fornece " . $hosts . " hosts livres";
         echo "<br>";
         echo "<br>";
 
-        $primeiro_host = primeiro_host_rede($ip4, $mascara);
-        echo "O primeiro host de rede é " . $primeiro_host . " ";
-         echo "<br>";
+        $privada_publica = privacidade_rede($ip1, $ip2);
+        echo "O endereço é " . $privada_publica;
         echo "<br>";
-
-        $ultimo_host = ultimo_host_rede($ip4, $mascara);
-        echo "O último host de rede é " . $ultimo_host . " ";
-         echo "<br>";
-        echo "<br>";
-
-        $mascara_decimal = calculo_mascara($mascara);
-        echo "A máscara decimal é" . " $mascara_decimal";
-         echo "<br>";
         echo "<br>";
 
         $resultado_classe = define_classe($ip1);
@@ -240,23 +224,36 @@ if ($_POST["acao"] == "enviar") {
         echo "<br>";
         echo "<br>";
 
-
-          
-
-        $privada_publica = privacidade_rede($ip1, $ip2);
-        echo "O endereço é " . $privada_publica;
-         echo "<br>";
+        $mascara_decimal = calculo_mascara($mascara);
+        echo "A máscara decimal é" . " $mascara_decimal";
         echo "<br>";
-                  
+        echo "<br>";
+       
+       // ---
 
         $enderecos_de_rede = enderecos_subrede($mascara);
-        $enderecos_de_broadcast = enderecos_broadcast($mascara);
+
+        $enderecos_de_broadcastt = enderecos_broadcast($mascara);
+        
         $c = 0;
         while ($c < $redes){
-        	$rede= $c+1;	
-            echo "Rede ". $rede." : ". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_rede[$c].'<br>';
-            echo "Broadcast ". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_broadcast[$c].'<br>';
+
+	        $enderecos_de_redee[$c] = $enderecos_de_rede[$c]+1;
+
+	        $enderecos_de_broadcast[$c] = $enderecos_de_broadcastt[$c]-1;                     
+
+            $rede= $c+1; 
+
+            echo "Sub-rede: ". $rede."<br>"."Endereço de rede: ". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_rede[$c].'<br>';
+
+            echo "Broadcast:". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_broadcastt[$c].'<br>';
+
+            echo "Primeiro host: ". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_redee[$c].'<br>';
+
+            echo "Ultimo host: ". $ip1.'.'.$ip2.'.'.$ip3.'.'.$enderecos_de_broadcast[$c].'<br>';
+
             echo "<br>";
+
             $c = $c + 1;
         }
         
